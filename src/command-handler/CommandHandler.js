804130f8ed15061bd1945c66600ed41c9e3357e2
VulgarBear <lucas@shisify.com>
2022-06-09 20:41:25 -0400

    added validations to syntax for commands and user input

---
+++ src/command-handler/CommandHandler.js
@@ -0,0 +1,80 @@
+const path = require('path')
+
+const getAllFiles = require('../util/get-all-files')
+const Command = require('./Command')
+
+class CommandHandler {
+  // <commandName, instance of the Command class>
+  commands = new Map()
+
+  constructor(commandsDir, client) {
+    this.commandsDir = commandsDir
+    this.readFiles()
+    this.messageListener(client)
+  }
+
+  readFiles() {
+    const files = getAllFiles(this.commandsDir)
+    const validations = this.getValidations('syntax')
+
+    for (let file of files) {
+      const commandObject = require(file)
+
+      let commandName = file.split(/[/\\]/)
+      commandName = commandName.pop()
+      commandName = commandName.split('.')[0]
+
+      const command = new Command(commandName, commandObject)
+
+      for (const validation of validations) {
+        validation(command)
+      }
+
+      this.commands.set(command.commandName, command)
+    }
+  }
+
+  messageListener(client) {
+    const validations = this.getValidations('run-time')
+
+    const prefix = 'hb.'
+
+    client.on('messageCreate', (message) => {
+      const { content } = message
+
+      if (!content.startsWith(prefix)) {
+        return
+      }
+
+      const args = content.split(/\s+/)
+      const commandName = args.shift().substring(prefix.length).toLowerCase()
+
+      const command = this.commands.get(commandName)
+      if (!command) {
+        return
+      }
+
+      const usage = { message, args, text: args.join(' ') }
+
+      for (const validation of validations) {
+        if (!validation(command, usage, prefix)) {
+          return
+        }
+      }
+
+      const { callback } = command.commandObject
+
+      callback(usage)
+    })
+  }
+
+  getValidations(folder) {
+    const validations = getAllFiles(
+      path.join(__dirname, `./validations/${folder}`)
+    ).map((filepath) => require(filepath))
+
+    return validations
+  }
+}
+
+module.exports = CommandHandler
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-const CommandHandler = require('./CommandHandler')
+const mongoose = require('mongoose')
+
+const CommandHandler = require('./command-handler/CommandHandler')
 
 class jotunnCommand {
-  constructor({ client, commandsDir }) {
+  constructor({ client, mongoUri, commandsDir }) {
     if (!client) {
       throw new Error('A client is required.')
+    }
+
+    if (mongoUri) {
+      this.connectToMongo(mongoUri)
     }
 
     if (commandsDir) {
       new CommandHandler(commandsDir, client)
     }
+  }
+
+  connectToMongo(mongoUri) {
+    mongoose.connect(mongoUri, {
+      keepAlive: true,
+    })
   }
 }
 
